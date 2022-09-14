7ae072e49e0cc5780386eaa76c25ef7b65545cc9
Alysson Marley Dias <callistomain@gmail.com>
2022-09-14 12:07:05 -0300

    Project start

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom';
+const root = document.getElementById('root');
+
+function App() {
+    return (
+        <div>
+            Hello World!
+        </div>
+    );
+}
+
+ReactDOM.render(<App/>, root);
